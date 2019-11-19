c4eb875c76bcea43fd54bf4524983271bb0c2e16
Julia Margarita Jimenez Herrera <maggie.jh99@gmail.com>
2019-11-19 16:23:25 -0600

    Constrains
    Adding autolayout

--- CulturaVisual/CulturaVisual/ViewController.swift
+++ CulturaVisual/CulturaVisual/ViewController.swift
@@ -11,12 +11,52 @@ import UIKit
 class ViewController: UIViewController {
     
     
-
+    @IBOutlet weak var lbQuizzlet: UILabel!
+    @IBOutlet weak var btTemas: UIButton!
+    @IBOutlet weak var btPerfil: UIButton!
+    @IBOutlet weak var btCreditos: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpLayout()
         // Do any additional setup after loading the view.
     }
 
+    func setUpLayout(){
+        
+        // Activa los constrains
+        btTemas.translatesAutoresizingMaskIntoConstraints = false
+        btPerfil.translatesAutoresizingMaskIntoConstraints = false
+        btCreditos.translatesAutoresizingMaskIntoConstraints = false
+        lbQuizzlet.translatesAutoresizingMaskIntoConstraints = false
+        
+        //Constrains del label
+        lbQuizzlet.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        lbQuizzlet.topAnchor.constraint(equalTo: view.topAnchor, constant: 170).isActive = true
+//        lbQuizzlet.widthAnchor.constraint(equalToConstant: 170).isActive = true
+//        lbQuizzlet.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        
+        
+        //Constrains del boton Temas
+        btTemas.topAnchor.constraint(equalTo: lbQuizzlet.bottomAnchor, constant: 80).isActive = true
+        btTemas.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+       btTemas.widthAnchor.constraint(equalToConstant: 190).isActive = true
+        btTemas.heightAnchor.constraint(equalToConstant: 75).isActive = true
+        
+        //Constarins del boton Perfil
+        btPerfil.topAnchor.constraint(equalTo: btTemas.bottomAnchor, constant: 40).isActive = true
+        btPerfil.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        btPerfil.widthAnchor.constraint(equalToConstant: 190).isActive = true
+        btPerfil.heightAnchor.constraint(equalToConstant: 75).isActive = true
+        
+       // Constrains del boton Creditos
+       btCreditos.topAnchor.constraint(equalTo: btPerfil.bottomAnchor, constant: 40).isActive = true
+        btCreditos.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    btCreditos.widthAnchor.constraint(equalToConstant: 190).isActive = true
+    btCreditos.heightAnchor.constraint(equalToConstant: 75).isActive = true
+        
+        
+    }
 
 }
 
--- CulturaVisual/CulturaVisual/ViewControllerLogin.swift
+++ CulturaVisual/CulturaVisual/ViewControllerLogin.swift
@@ -12,14 +12,19 @@ class ViewControllerLogin: UIViewController {
         
     @IBOutlet weak var lbEmail: UITextField!
     @IBOutlet weak var lbPassword: UITextField!
+    @IBOutlet weak var lbLoging: UILabel!
+    @IBOutlet weak var lbContra: UILabel!
+    @IBOutlet weak var lbCorreo: UILabel!
+    @IBOutlet weak var btLogIn: UIButton!
+    @IBOutlet weak var btSignUp: UIButton!
+    
     let userDefaults = UserDefaults.standard
 
     
     //------------------------------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        // Do any additional setup after loading the view.
+        setUpLayout()
     }
     
     //------------------------------------------------------
@@ -53,6 +58,54 @@ class ViewControllerLogin: UIViewController {
                 }
             }
         }
+    }
+    
+    func setUpLayout() {
+        //This enables autolayout
+        lbLoging.translatesAutoresizingMaskIntoConstraints = false
+        lbEmail.translatesAutoresizingMaskIntoConstraints = false
+        lbPassword.translatesAutoresizingMaskIntoConstraints = false
+        lbContra.translatesAutoresizingMaskIntoConstraints = false
+        lbContra.translatesAutoresizingMaskIntoConstraints = false
+        btSignUp.translatesAutoresizingMaskIntoConstraints = false
+        btLogIn.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Contrains del label Login
+        lbLoging.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        lbLoging.centerYAnchor.constraint(equalTo: view.topAnchor, constant: 170).isActive = true
+//        lbLoging.widthAnchor.constraint(equalToConstant: 100).isActive = true
+//      lbLoging.heightAnchor.constraint(equalToConstant: 50).isActive = true
+       
+        // Contrains del texf field Email
+        lbEmail.widthAnchor.constraint(equalToConstant: 140).isActive = true
+        lbEmail.topAnchor.constraint(equalTo: lbLoging.bottomAnchor, constant: 125).isActive = true
+        lbEmail.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 160).isActive = true
+        
+        // Contrains del text field Password
+        lbPassword.widthAnchor.constraint(equalToConstant: 140).isActive = true
+        lbPassword.topAnchor.constraint(equalTo: lbEmail.bottomAnchor, constant: 25).isActive = true
+        lbPassword.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 160).isActive = true
+        
+        // Constrains del label Correo
+        lbCorreo.topAnchor.constraint(equalTo: lbLoging.bottomAnchor, constant: 128).isActive = true
+        lbCorreo.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 100).isActive = true
+        
+        // Constrains del label Contraseña
+        lbContra.topAnchor.constraint(equalTo: lbCorreo.bottomAnchor, constant: 40).isActive = true
+        lbContra.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 50).isActive = true
+        
+        //Contrains del boton LogIn
+        btLogIn.topAnchor.constraint(equalTo: lbPassword.bottomAnchor, constant: 90).isActive = true
+        btLogIn.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        btLogIn.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        btLogIn.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        //Constrains del boton SignUp
+        btSignUp.topAnchor.constraint(equalTo: btLogIn.bottomAnchor, constant: 30).isActive = true
+        btSignUp.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        btSignUp.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        btSignUp.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
     }
     /*
     // MARK: - Navigation
