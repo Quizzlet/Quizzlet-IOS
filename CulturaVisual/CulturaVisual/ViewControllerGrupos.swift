f92e744a1e3ab18f74938b90396fbb55b61fee15
Angel Odiel Trevino Villanueva <angel.trevinov@gmail.com>
2019-11-20 15:40:54 -0600

    Adding Pop Over

--- CulturaVisual/CulturaVisual/ViewControllerGrupos.swift
+++ CulturaVisual/CulturaVisual/ViewControllerGrupos.swift
@@ -11,7 +11,8 @@ import UIKit
 class ViewControllerGrupos:
     UIViewController,
     UITableViewDelegate,
-UITableViewDataSource {
+    UITableViewDataSource,
+    UIPopoverPresentationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +25,6 @@ UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
-    
     //------------------------------------------------------
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "celda", for: indexPath)
@@ -32,16 +32,23 @@ UITableViewDataSource {
         return cell
     }
     
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - PopOver
+    //------------------------------------------------------
+    func adaptivePresentationStyle(
+           for controller: UIPresentationController
+       ) -> UIModalPresentationStyle {
+           return .none
     }
-    */
-
+    
+    // MARK: - Navigation
+    //------------------------------------------------------
+    override func prepare(
+        for segue: UIStoryboardSegue,
+        sender: Any?
+    ) {
+        if(segue.identifier == "PopUp") {
+            let vistaPopOver = segue.destination as! ViewControllerPop
+            vistaPopOver.popoverPresentationController?.delegate = self
+        }
+    }
 }
--- CulturaVisual/CulturaVisual/ViewControllerPop.swift
+++ CulturaVisual/CulturaVisual/ViewControllerPop.swift
@@ -10,21 +10,28 @@ import UIKit
 
 class ViewControllerPop: UIViewController {
 
+    //------------------------------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        preferredContentSize = CGSize(
+            width: 250,
+            height: 200
+        )
     }
     
-
-    /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //------------------------------------------------------
+    override func prepare(
+        for segue: UIStoryboardSegue,
+        sender: Any?
+    ) {
+        
     }
-    */
+    //------------------------------------------------------
+    @IBAction func popoverPresentationControllerDidDismissPopover(
+        popoverPresentationController: UIPopoverPresentationController
+    ) {
+        dismiss(animated: true, completion: nil)
+    }
 
 }
